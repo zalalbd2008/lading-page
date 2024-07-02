18ac2b9552c23f4e07e229b9f2531c2a91324c29
Abid Hasan kiam <kiamhasan267@gmail.com>
2024-07-02 20:26:19 +0600

    testing -1

--- src/features/theme/components/theme.tsx
+++ src/features/theme/components/theme.tsx
@@ -5,12 +5,6 @@ import { Moon, Sun } from 'lucide-react';
 import { useTheme } from 'next-themes';
 
 import { Button } from '@/components/ui/button';
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
 
 export function ModeToggle() {
   const { setTheme } = useTheme();
@@ -25,6 +19,7 @@ export function ModeToggle() {
       aria-checked="false"
       data-state="unchecked"
       value="on"
+      aria-label="button"
     >
       <Sun
         className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
